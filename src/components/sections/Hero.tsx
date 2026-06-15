1e78059e508a330ced4c7e7ae5ce2bab596d7a30
jahanzaib99 <jahanzaibwork99@gmail.com>
2026-06-16 00:38:49 +0500

    resume added

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -67,7 +67,7 @@ export function Hero() {
             <ArrowRight className="ml-2 h-4 w-4 transition-transform group-hover:translate-x-1" />
           </Button>
           <a 
-            href="/Jahanzaib Resume.pdf" 
+            href="/Jahanzaib CV.pdf" 
             download="Muhammad_Jahanzaib_Resume.pdf"
             className="w-full sm:w-auto"
           >
